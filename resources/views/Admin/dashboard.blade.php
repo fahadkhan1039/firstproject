754585224924a44a727077b3876bb2012ed06e74
fahadkhan1039 <kmuhammadfahad496@gmail.com>
2025-01-02 23:10:54 +0500

    data

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -101,12 +101,14 @@
 @endsection
 
 @push('js')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.all.min.js"></script>
 <script>
     @if(session('success'))
         Swal.fire({
             icon: 'success',
             title: 'Success!',
             text: '{{ session('success') }}',
+            confirmButtonColor: '#4CAF50',
         });
     @endif
 
@@ -115,6 +117,7 @@
             icon: 'info',
             title: 'Info!',
             text: '{{ session('info') }}',
+            confirmButtonColor: '#3085d6',
         });
     @endif
 </script>
